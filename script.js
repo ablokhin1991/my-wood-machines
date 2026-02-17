3862b25daddc80d234d3eea171ae2012402a7ea1
ablokhin1991 <a.blokhin1991@gmail.com>
2026-02-17 13:07:49 +0300

    Обновление проекта

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
           </span>
           <span class="card-old-price">${formatPrice(machine.oldPrice)}</span>
         </div>
-        <div class="card-price-note">Цена и наличие уточняйте по телефону. Не является публичной офертой.</div>
+        <!--<div class="card-price-note">Цена и наличие уточняйте по телефону. Не является публичной офертой.</div>-->
         <h3 class="card-title" itemprop="name">${machine.name}</h3>
         <div class="card-instock">В наличии: ${machine.instock} шт.</div>
         <p class="card-desc" itemprop="description">${shortDesc}</p>
